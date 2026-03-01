5c96cb73b08de7f8a45ee8452a60aaaed8cc848b
adrita06 <adritaahsan06@gmail.com>
2026-03-01 11:35:46 +0600

    updated cnn training code

--- src/models/baseline/train_hf_cnn.py
+++ src/models/baseline/train_hf_cnn.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import os
 
 from src.models.baseline.hf_cnn_resnet import HFCNNResNet
 from src.preprocessing.hf_dataloader import get_dataloaders
@@ -20,7 +21,9 @@ train_loader, val_loader = get_dataloaders(
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
-num_epochs = 1
+num_epochs = 7
+
+best_loss = float("inf")
 
 for epoch in range(num_epochs):
     model.train()
@@ -36,7 +39,31 @@ for epoch in range(num_epochs):
         optimizer.step()
 
         total_loss += loss.item()
+    
+    avg_loss = total_loss / len(train_loader)
+    print(f"Epoch {epoch+1}, Loss: {avg_loss:.4f}")
 
-    print(f"Epoch {epoch+1}, Loss: {total_loss/len(train_loader):.4f}")
+    model.eval()
+    val_loss = 0
 
-torch.save(model.state_dict(), "trained_models/hf_cnn_resnet.pth")
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+
+    val_loss /= len(val_loader)
+    print(f"Validation Loss: {val_loss:.4f}")
+
+    save_path = "/content/data/AlertDrive/trained_model"
+    os.makedirs(save_path, exist_ok=True)
+
+    # Save every epoch
+    torch.save(model.state_dict(), f"{save_path}/epoch_{epoch+1}.pth")
+
+    # Best model based on validation loss
+    if val_loss < best_loss:
+        best_loss = val_loss
+        torch.save(model.state_dict(), f"{save_path}/best_model.pth")
+        print("Best model updated based on validation loss!")
